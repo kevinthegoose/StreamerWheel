3b19ca78692d07349daed681d6061f1c0f2e3746
kevinthegoose <kevin.christiansen87@gmail.com>
2023-04-03 12:34:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -70,77 +70,23 @@ function drawSlice(sliceNumber, sliceCount, sliceSize, ctx, canvasWidth, canvasH
   var angle = sliceNumber * sliceSize;
 
   // Set the color and fill of the slice
-  ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16); // generate random color
+  ctx.fillStyle = "#" + ((1 << 24) * Math.random() | 0).toString(16); // generate random color
   ctx.beginPath();
   ctx.moveTo(canvasWidth / 2, canvasHeight / 2);
   ctx.arc(canvasWidth / 2, canvasHeight / 2, canvasWidth / 2, angle * Math.PI / 180, (angle + sliceSize) * Math.PI / 180);
   ctx.lineTo(canvasWidth / 2, canvasHeight / 2);
-  ctx.fill();
+ctx.closePath();
+ctx.fill();
 
-  // Add the character image to the slice
-  var img = new Image();
-  img.src = imageURL;
-  img.onload = function() {
-    ctx.save();
-    ctx.translate(canvasWidth / 2, canvasHeight / 2);
-    ctx.rotate((angle + sliceSize / 2) * Math.PI / 180);
-    ctx.drawImage(img, -canvasWidth / 4, -canvasHeight / 2 + 10, canvasWidth / 2, canvasHeight / 2);
-    ctx.restore();
-  };
+// Draw the image on the slice
+var image = new Image();
+image.src = imageURL;
+image.onload = function() {
+var size = Math.min(canvasWidth, canvasHeight) / sliceCount;
+var x = canvasWidth / 2 + Math.cos(angle + sliceSize / 2) * canvasWidth / 4;
+var y = canvasHeight / 2 + Math.sin(angle + sliceSize / 2) * canvasHeight / 4;
+ctx.drawImage(image, x - size / 2, y - size / 2, size, size);
+}
 }
 
-function spinWheel() {
-  // Set the spin time and spin angle
-  spinTimeTotal = Math.floor(Math.random() * 4000 + 5000); // random spin time between 5 and 9 seconds
-  spinAngleStart = Math.random() * 360;
-
-  // Call the spin function
-  spin();
-}
-
-function spin() {
-  spinTimeElapsed += 30;
-
-  if (spinTimeElapsed >= spinTimeTotal) {
-    stopSpin();
-    return;
-  }
-
-  var spinAngle = spinAngleStart - easeOut(spinTimeElapsed, 0, spinAngleStart, spinTimeTotal);
-  drawSpinner(spinAngle);
-  spinTimeout = setTimeout(spin, 30);
-}
-
-function drawSpinner(degrees) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.save();
-  ctx.translate(canvas.width /   2, canvas.height / 2);
-  ctx.rotate(degrees * Math.PI / 180);
-  ctx.strokeStyle = "#ffffff";
-  ctx.lineWidth = 2;
-  ctx.beginPath();
-  ctx.moveTo(-20, 0);
-  ctx.lineTo(20, 0);
-  ctx.stroke();
-  ctx.restore();
-}
-
-function stopSpin() {
-  clearTimeout(spinTimeout);
-  spinAngleStart = 0;
-}
-
-function easeOut(t, b, c, d) {
-  var ts = (t /= d) * t;
-  var tc = ts * t;
-  return b + c * (tc + -3 * ts + 3 * t);
-}
-
-// Add event listener for the spin button
-var spinButton = document.getElementById("spin-button");
-spinButton.addEventListener("click", function() {
-  spinWheel();
-});
-
-
-                
+createWheel(); // call the createWheel function to draw the wheel and its slices.
