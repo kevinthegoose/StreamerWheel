1c2186e990ca95f287da5ab35f0f73724058e667
kevinthegoose <kevin.christiansen87@gmail.com>
2023-04-03 14:10:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-function myfunction(){
+let container = document.querySelector(".container");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 1000);
 
-
-	var x = 1024; //min value
-	var y = 9999; // max value
-
-	var deg = Math.floor(Math.random() * (x - y)) + y;
-
-	document.getElementById('box').style.transform = "rotate("+deg+"deg)";
-
-	var element = document.getElementById('mainbox');
-	element.classList.remove('animate');
-	setTimeout(function(){
-		element.classList.add('animate');
-	}, 5000); //5000 = 5 second
+btn.onclick = function () {
+  container.style.transform = "rotate(" + number + "deg)";
+  number += Math.ceil(Math.random() * 1000);
 }
