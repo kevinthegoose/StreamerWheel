b898b27d6e1f9cfb3619453ae4a182699298685b
kevinthegoose <kevin.christiansen87@gmail.com>
2023-04-03 14:51:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-let container = document.querySelector(".container");
-let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 1000);
+const spinBtn = document.querySelector("#spin");
+const container = document.querySelector(".container");
 
-btn.onclick = function () {
-  container.style.transform = "rotate(" + number + "deg)";
-  number += Math.ceil(Math.random() * 1000);
+function spin() {
+  const deg = Math.floor(Math.random() * 2000) + 1000;
+  container.style.transform = `rotate(${deg}deg)`;
+
+  setTimeout(() => {
+    const allDivs = document.querySelectorAll(".container div");
+    let winnerIndex = null;
+    allDivs.forEach((div, index) => {
+      const rect = div.getBoundingClientRect();
+      if (
+        rect.x + rect.width / 2 > container.clientWidth / 2 &&
+        rect.y + rect.height / 2 > container.clientHeight / 2
+      ) {
+        winnerIndex = index + 1;
+      }
+    });
+
+    alert(`Winner is section ${winnerIndex}`);
+    container.style.transform = "none";
+  }, 5000);
 }
+
+spinBtn.addEventListener("click", spin);
